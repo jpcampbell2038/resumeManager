d7e3b3e13305094cb10dbfa0ecd2c50c6f021891
jpcampbell2038 <[jpcampbell@crimson.ua.edu]>
2020-08-03 10:24:15 -0500

    Updated for submission

--- src/app.js
+++ src/app.js
@@ -3,17 +3,15 @@ import ReactDOM from 'react-dom';
 import { Provider } from 'react-redux';
 import AppRouter from './routers/AppRouter';
 import configureStore from './store/configureStore';
-import { setTextFilter, sortByStatus, sortByDate, sortByGradDate, setStartDate, setEndDate } from './actions/filters';
-import { getVisibleResumes } from './selectors/resumes';
 import 'normalize.css/normalize.css';
 import './styles/styles.scss';
-import { addResume, removeResume, editResume } from './actions/resumes';
+import { addResume } from './actions/resumes';
 
 const store = configureStore();
 
-store.dispatch(addResume({ studentName: 'james', status: 'b', graduationDate: 1, createdAt: 1000 }));
-store.dispatch(addResume({ studentName: 'bandit', status: 'a', graduationDate: 2, createdAt: -1000 }));
-store.dispatch(addResume({ studentName: 'jim', status: 'c', graduationDate: 3, createdAt: 2000 }));
+store.dispatch(addResume({ studentName: 'james', status: 'Needs Revision', graduationDate: 1, createdAt: 1000 }));
+store.dispatch(addResume({ studentName: 'bandit', status: 'Reviewed', graduationDate: 2, createdAt: -1000 }));
+store.dispatch(addResume({ studentName: 'jim', status: 'Needs Revision', graduationDate: 3, createdAt: 2000 }));
 
 const state = store.getState();
 console.log(state);
